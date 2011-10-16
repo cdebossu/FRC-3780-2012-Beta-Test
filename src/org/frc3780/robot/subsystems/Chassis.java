53e4bbe874ab620b7f4a9bf12c95466f70f6ae99
Oliver Graff <ograff@frc3780.org>
2011-10-15 23:00:41 -0400

    Make use of the Shifting Drive Train

--- src/org/frc3780/robot/subsystems/Chassis.java
+++ src/org/frc3780/robot/subsystems/Chassis.java
@@ -6,22 +6,29 @@
 package org.frc3780.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Jaguar;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.frc3780.robot.OI;
 import org.frc3780.robot.commands.DriveWithJoystick;
+import org.frc3780.robot.components.ServoSuperShifter;
+import org.frc3780.robot.components.ShiftingRobotDrive;
 
 /**
  * @author Oliver Graff (Team 3780)
  */
 
 public class Chassis extends Subsystem {
-    public static final int RIGHT_MOTOR_PORT = 8;
-    public static final int LEFT_MOTOR_PORT = 1;
+    public static final int RIGHT_MOTOR_PORT_1 = 8;
+    public static final int RIGHT_MOTOR_PORT_2 = 8;
+    public static final int RIGHT_SHIFTER_PORT = 9;
+            
+    public static final int LEFT_MOTOR_PORT_1 = 1;
+    public static final int LEFT_MOTOR_PORT_2 = 1;
+    public static final int LEFT_SHIFTER_PORT = 10;
+    
     private static Chassis instance = null;
 
-    private RobotDrive drive;
+    private ShiftingRobotDrive drive;
     public static Chassis getInstance() {
         if(instance == null) {
             instance = new Chassis();
@@ -34,7 +41,7 @@ public class Chassis extends Subsystem {
 
     // Initialize your subsystem here
     private Chassis() {
-        drive = new RobotDrive(new Jaguar(LEFT_MOTOR_PORT), new Jaguar(RIGHT_MOTOR_PORT));
+        drive = new ShiftingRobotDrive(new ServoSuperShifter(new Jaguar(LEFT_MOTOR_PORT_1), new Jaguar(LEFT_MOTOR_PORT_2), new Servo(LEFT_SHIFTER_PORT)), new ServoSuperShifter(new Jaguar(RIGHT_MOTOR_PORT_1), new Jaguar(RIGHT_MOTOR_PORT_2), new Servo(RIGHT_SHIFTER_PORT)));
         drive.setSafetyEnabled(false);
         
     }
@@ -43,7 +50,26 @@ public class Chassis extends Subsystem {
      */
     public void driveWithJoystick() {
         drive.arcadeDrive(OI.getInstance().getDriveJoystick());
-        
     }
+    /**
+     * Gets the current gear
+     * @return The current gear
+     */
+    public int getGear() {
+        return drive.getGear();
+    }
+    /**
+     * Shifts the Drive up
+     */
+    public void shiftUp() {
+        drive.shiftUp();
+    }
+    /**
+     * Shifts the Drive down
+     */
+    public void shiftDown() {
+        drive.shiftDown();
+    }
+    
 }
 

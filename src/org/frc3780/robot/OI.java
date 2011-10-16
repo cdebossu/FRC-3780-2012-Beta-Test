ed090332b8dd0550e0f543bcadfcde9d2f6d8743
Oliver Graff <ograff@frc3780.org>
2011-10-15 23:02:02 -0400

    Tied Shift up and Down commands to Buttons

--- src/org/frc3780/robot/OI.java
+++ src/org/frc3780/robot/OI.java
@@ -2,14 +2,28 @@
 package org.frc3780.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+/**
+ * 
+ * @author Oliver Graff (Team 3780)
+ */
 public class OI {
     private static final int DRIVE_JOYSTICK_PORT = 1;
     private static OI instance = null;
     
     private Joystick driveStick;
+    
+    public final Button shiftUpButton;
+    public final Button shiftDownButton;
     private OI() {
         driveStick = new Joystick(DRIVE_JOYSTICK_PORT);
+        
+        shiftUpButton = new JoystickButton(driveStick, 3);
+        shiftDownButton = new JoystickButton(driveStick, 4);
+         
+        shiftDownButton.whenPressed(new ShiftDown());
+        shiftUpButton.whenPressed(new ShiftUp());
     }
     
     public static OI getInstance() {

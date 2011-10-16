83d550ecd23e2c1db8269610a91be5364744842f
Oliver Graff <ograff@frc3780.org>
2011-10-16 00:47:55 -0400

    Added highest and lowest gear constants

--- src/org/frc3780/robot/Global.java
+++ src/org/frc3780/robot/Global.java
@@ -6,5 +6,8 @@ package org.frc3780.robot;
  * @author Oliver Graff (Team 3780)
  */
 public class Global {
-    public static boolean DEBUG = true;
+    public static final boolean DEBUG = true;
+    
+    public static final int HIGHEST_GEAR = 2;
+    public static final int LOWEST_GEAR = 1;
 }
